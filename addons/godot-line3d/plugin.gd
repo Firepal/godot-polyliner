f7d9b2bcb74f9b1d0203ad3b26b019054bee300f
Firepal <badbrad@outlook.fr>
2021-10-13 23:44:56 +0200

    Strip unused UI code

--- addons/godot-line3d/plugin.gd
+++ addons/godot-line3d/plugin.gd
@@ -1,19 +1,12 @@
 tool
 extends EditorPlugin
 
-var l = MenuButton.new()
+# TODO: Use add_control_to_container to allow Editor export of line meshes.
+
 func _enter_tree():
-	l.text = "yese"
-#	var p = l.get_popup()
-#	p.add_item("yes")
-	add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU,l)
-	add_custom_type("Line3D","Path",preload("Line3D.gd"),preload("icon.png"))
+	add_custom_type("Line3D","Path",preload("Line3D/Line3D.gd"),preload("Line3D/icon.png"))
 	make_visible(false)
 
-func make_visible(visible):
-	if l:
-		l.visible = visible
 
 func _exit_tree():
-	remove_control_from_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU,l)
 	remove_custom_type("Line3D")
